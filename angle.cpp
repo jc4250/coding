da2e018cf28cf1b511a6fdee4de5cf88a7f10e55
Jaydeep Chauhan <cjaydeep69@gmail.com>
2021-05-04 23:38:07 +0530

    1344. Angle Between Hands of a Clock

---
+++ angle.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/angle-between-hands-of-a-clock/
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        
+        double agm = (1.0 * 360 * minutes)/(60);
+        
+        double orh = ((1.0 * minutes)/(60)) + hour;
+        
+        if (orh >= 12) {
+            orh -= 12;
+        }
+        
+        double agh = (1.0 * 30 * orh);
+        
+        return min(abs(agm-agh), 360 - abs(agm-agh));
+    }
+};
